e1a4e9f3c115b17292dd2debbfe54ca40974da7a
cttv2 <46335918+cttv2@users.noreply.github.com>
2020-02-23 19:27:33 +0800

    Add files via upload

---
+++ day17-18/code1/code1.js
@@ -0,0 +1,11 @@
+function sanbeishu(number){
+    var i;
+    for(i=1;i<number;i++){
+        if(i%3===0){
+            console.log('PA');
+        }else{
+            console.log(i);
+        }
+    }
+}
+
